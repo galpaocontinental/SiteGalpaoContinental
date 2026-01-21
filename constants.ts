4fb44c1663a44be4f691825c6d45f1e9827d69b3
Diego Sanches <contato@polygonconsulting.com.br>
2026-01-21 12:55:04 -0300

    Update image paths and contact information

--- constants.ts
+++ constants.ts
@@ -7,12 +7,13 @@ export const IMAGES = {
   AMSTEL: 'https://lh3.googleusercontent.com/aida-public/AB6AXuDlWeLg3s9QoSJy3UwJUhYQVIOi_-Fp76Jh_TmJg-s0qziL87EIm8TzQoG9zjWg8D0OHHDacLyOhTlO4puHuMsnMarfi3l5ndHvUZyt2r1ZmYTeiJivZ9_RDwYEbrzBSlKaXwjWlPhAoJ9a-tRnODu-GzEdKKoQ4702gDVkggjGaKZoVKrI7FAWbB1Mllz-Pdl1c7rl3kd8a84Bx9_Su6mV8fp5JhxbNdlBZ25paUA3G4EnA-HbFkAglt1Wo9XzSO_iYpjP1vCmk2c',
   ORIGINAL: withBase('images/original.png'),
   SPATEN: withBase('images/spaten.png'),
-  ABOUT_HERO: withBase('images/about-hero.jpg'), // Adicione esta imagem
+  ABOUT_HERO: withBase('images/about_hero.jpeg'), // Adicione esta imagem
   MAP: withBase('images/map.png'), // Adicione esta imagem
 };
 
 export const CONTACT_INFO = {
-  PHONE: '(11) 98765-4321',
+  PHONE: '(11) 96404-2721,
+  WHATSAPP: '(11) 96404-2721,
   EMAIL: 'contato@galpaocontinental.com.br',
-  ADDRESS: 'Rua Exemplo, 123 - São Paulo, SP'
+  ADDRESS: 'Av. Francisco Etorre Pedro Mari, 555 - Taboão da Serra, SP'
 };
